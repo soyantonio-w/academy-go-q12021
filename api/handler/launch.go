0bd3071c9f0729bf4afa89ff4277b262e9edd36f
soyantonio-w <jesus.perez@wizeline.com>
2021-03-04 01:26:20 -0600

    Return all launches

--- api/handler/launch.go
+++ api/handler/launch.go
@@ -1,18 +1,28 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/soyantonio-w/academy-go-q12021/api/presenter"
-	"github.com/soyantonio-w/academy-go-q12021/entity"
+	"github.com/soyantonio-w/academy-go-q12021/infrastructure/repository/csv"
 	"net/http"
 	"strconv"
 )
 
 func GetLaunch(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(request)
 	launchId, _ := strconv.Atoi(vars["id"])
-	p := presenter.LaunchPresenter{ ID: entity.LaunchId(launchId) }
 
-	writer.Write(p.Format())
-	writer.Header().Set("Content-Type", "application/json")
+	r := csv.NewRepository()
+	launches, _ := r.GetLaunches()
+
+	var presenters []presenter.LaunchPresenter
+	for _, launch := range launches {
+		p := presenter.NewLaunchPresenter(launch)
+		presenters = append(presenters, p)
+	}
+
+	fmt.Println(launchId)
+	writer.Write(presenter.FormatMany(presenters))
 }
