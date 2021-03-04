5237f1c536a47f30e78d914167f3d2de12acbed8
soyantonio-w <jesus.perez@wizeline.com>
2021-03-04 00:44:24 -0600

    Add constructor of a launch

--- entity/launch.go
+++ entity/launch.go
@@ -19,3 +19,20 @@ func ToLaunchId(id int) LaunchId {
 	var algo LaunchId = LaunchId(id)
 	return algo
 }
+
+type LaunchRepo interface {
+	GetLaunches() ([]Launch, error)
+}
+
+func NewLaunch(launchId int, details, date, missionName, videoLink, rocketName string, launchSuccess bool) (l Launch) {
+	l = Launch{
+		LaunchId: LaunchId(launchId),
+		LaunchDetails: LaunchDetails(details),
+		LaunchDate: LaunchDate(date),
+		LaunchSuccess: LaunchSuccess(launchSuccess),
+		VideoLink: VideoLink(videoLink),
+		RocketName: RocketName(rocketName),
+		MissionName: MissionName(missionName),
+	}
+	return
+}
