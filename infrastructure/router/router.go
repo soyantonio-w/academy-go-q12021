5e4ef0a9f60ec895f17a8a900288854b716159a4
soyantonio-w <jesus.perez@wizeline.com>
2021-03-30 16:45:44 -0600

    feat: add filter endpoint

--- infrastructure/router/router.go
+++ infrastructure/router/router.go
@@ -11,6 +11,7 @@ type Handler interface {
 	GetLaunch(w http.ResponseWriter, r *http.Request)
 	ListLaunches(w http.ResponseWriter, r *http.Request)
 	SyncLaunches(w http.ResponseWriter, r *http.Request)
+	FilterLaunches(w http.ResponseWriter, r *http.Request)
 }
 
 // New - creates a mux router
@@ -20,6 +21,13 @@ func New(flight Handler, cache Handler) *mux.Router {
 	r.HandleFunc("/cache/launch/{id:[0-9]+}", cache.GetLaunch).Methods("GET")
 	r.HandleFunc("/cache/launches/sync", cache.SyncLaunches).Methods("GET")
 	r.HandleFunc("/cache/launches/list", cache.ListLaunches).Methods("GET")
+	r.HandleFunc("/cache/launches/filter", cache.FilterLaunches).
+		Queries(
+			"type", "{type:odd|even}",
+			"items", "{items:[0-9]+}",
+			"items_per_workers", "{items_per_workers:[0-9]+}",
+		).
+		Methods("GET")
 
 	r.HandleFunc("/flight/launch/{id:[0-9]+}", flight.GetLaunch).Methods("GET")
 	r.HandleFunc("/flight/launches/list", flight.ListLaunches).Methods("GET")
