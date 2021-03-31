a2fdb3c766303b0a4edb4172451457f2f232a016
soyantonio-w <jesus.perez@wizeline.com>
2021-03-30 20:42:28 -0600

    feat: add workers to execute the filtering

--- usecase/launch.go
+++ usecase/launch.go
@@ -2,7 +2,10 @@ package usecase
 
 import (
 	"fmt"
+	"log"
+	"math"
 	"strconv"
+	"sync"
 
 	"github.com/soyantonio-w/academy-go-q12021/entity"
 )
@@ -44,10 +47,104 @@ func (s *LaunchUseCase) FilterLaunches(filterType string, items int, itemsPerWor
 	if err != nil {
 		return []entity.Launch{}, err
 	}
+	if itemsPerWorker <= 0 {
+		return []entity.Launch{}, fmt.Errorf("items per worker should be greater than 0")
+	}
 
+	totalLaunches := len(launches)
 	t := NewLaunchFilterType(filterType)
+	inRangeItems := int(math.Min(float64(items), float64(totalLaunches)))
+	workers := calculateRequiredWorkers(totalLaunches, itemsPerWorker)
+	results := make(chan entity.Launch)
+	done := make(chan int, workers)
 
-	return filterLaunchesByType(launches, *t), nil
+	var wg sync.WaitGroup
+	wg.Add(workers)
+	shutdown := make(chan struct{})
+
+	for w := 0; w < workers; w++ {
+		i := &interval{
+			iteration: w,
+			items:     itemsPerWorker,
+			length:    totalLaunches,
+		}
+		start, end := i.getIntervals()
+		go worker(&wg, done, shutdown, w, launches[start:end], results, t)
+	}
+
+	filteredLaunches := filterLaunches(results, done, workers, inRangeItems)
+
+	log.Printf("wating %d", workers)
+	close(shutdown)
+	wg.Wait()
+	log.Printf("done")
+
+	return filteredLaunches, nil
+}
+
+func filterLaunches(results chan entity.Launch, done chan int, workers, items int) []entity.Launch {
+	workersDone := 0
+	filteredLaunches := make([]entity.Launch, 0, items)
+	if workers == 0 || items == 0 {
+		return filteredLaunches
+	}
+	for {
+		select {
+		case launch := <-results:
+			filteredLaunches = append(filteredLaunches, launch)
+
+			if len(filteredLaunches) == items {
+				return filteredLaunches
+			}
+		case <-done:
+			workersDone += 1
+			if workersDone == workers {
+				return filteredLaunches
+			}
+		}
+	}
+}
+
+func calculateRequiredWorkers(items int, itemsPerWorker int) int {
+	return int(math.Ceil(float64(items) / float64(itemsPerWorker)))
+}
+
+func worker(
+	wg *sync.WaitGroup,
+	workerDone chan int,
+	shutdown chan struct{},
+	id int, launches []entity.Launch,
+	results chan<- entity.Launch,
+	filterType *LaunchFilterType,
+) {
+	log.Printf("worker %d started job", id)
+
+	filteredLaunches := filterLaunchesByType(launches, *filterType)
+	if len(filteredLaunches) == 0 {
+		log.Printf("worker %d finished job", id)
+		workerDone <- id
+		wg.Done()
+		return
+	}
+
+	i := 0
+	for {
+		select {
+		case results <- filteredLaunches[i]:
+			i++
+			if i == len(filteredLaunches) {
+				log.Printf("worker %d finished job", id)
+				workerDone <- id
+				wg.Done()
+				return
+			}
+		case <-shutdown:
+			workerDone <- id
+			wg.Done()
+			log.Printf("worker %d finished job wii", id)
+			return
+		}
+	}
 }
 
 // ListLaunches - loads all the launches from the provided launch use case
